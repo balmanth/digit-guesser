9624f609e1f0fa7cbc10d406ac4cebfd35132e14
Silas B. Domingos <silas@balmante.eti.br>
2022-01-09 09:17:45 -0300

    Add enhanced worker

--- src/core/worker.ts
+++ src/core/worker.ts
@@ -1,28 +1,64 @@
 /**
- * Allowed worker value types.
+ * Allowed value types.
  */
-export type WorkerValueTypes = boolean | number | bigint | string | WorkerValueTypes[];
+type ValueTypes = boolean | number | bigint | string | ValueTypes[];
+
+/**
+ * Event callback type.
+ */
+type EventCallback = (...values: ValueTypes[]) => ValueTypes | void | Promise<ValueTypes | void>;
+
+/**
+ * Map of event listeners.
+ */
+type EventListenersMap = {
+  [name: string]: EventCallback[];
+};
+
+/**
+ * Map of event promises.
+ */
+type EventPromisesMap = {
+  [id: number]: {
+    /**
+     * Resolve callback.
+     */
+    resolve: (values: ValueTypes[]) => void;
+    /**
+     * Reject callback.
+     */
+    reject: (values: string) => void;
+  };
+};
+
+/**
+ * Worker value types.
+ */
+export type WorkerValueTypes = ValueTypes;
 
 /**
  * Worker event callback type.
  */
-export type WorkerEventCallback = (...values: WorkerValueTypes[]) => void;
-
-/**
- * Map of events.
- */
-type EventMap = {
-  [name: string]: WorkerEventCallback[];
-};
+export type WorkerEventCallback = EventCallback;
 
 /**
  * Worker wrapper class.
  */
 export class Worker {
   /**
-   * Worker events.
+   * Counter for promises Ids.
    */
-  #events: EventMap = {};
+  static #promiseIds = 0;
+
+  /**
+   * Map of event promises.
+   */
+  static #eventPromises: EventPromisesMap = [];
+
+  /**
+   * Map of event listeners.
+   */
+  #eventListeners: EventListenersMap = {};
 
   /**
    * Window thread.
@@ -30,17 +66,68 @@ export class Worker {
   #thread: Window;
 
   /**
-   * Handle worker message events from the window thread.
+   * Assert the event name is not empty.
+   * @param name Event name.
+   */
+  static #assertName(name: string): void {
+    if (name.length === 0) {
+      throw new Error(`Event name can't be empty.`);
+    }
+  }
+
+  /**
+   * Notify all the event listeners that corresponds to the given name.
+   * @param id Event Id.
+   * @param name Event name.
+   * @param values Event values.
+   */
+  async #notifyListeners(id: number, name: string, ...values: ValueTypes[]): Promise<void> {
+    const events = this.#eventListeners[name];
+    if (events !== void 0) {
+      try {
+        let results = [];
+        for (const callback of events) {
+          const result = await callback(...values);
+          if (result !== void 0) {
+            results.push(result);
+          }
+        }
+        this.#thread.postMessage([id, '', true, ...results]);
+      } catch (error) {
+        this.#thread.postMessage([id, '', false, `Worker: ${error}`]);
+      }
+    }
+  }
+
+  /**
+   * Notify the event promise that corresponds to the given Id.
+   * @param id Event Id.
+   * @param values Event values.
+   */
+  #notifyPromises(id: number, ...values: ValueTypes[]): void {
+    const promise = Worker.#eventPromises[id];
+    if (promise !== void 0) {
+      delete Worker.#eventPromises[id];
+      const [status, ...results] = values;
+      if (status) {
+        promise.resolve(results);
+      } else {
+        promise.reject(results[0] as string);
+      }
+    }
+  }
+
+  /**
+   * Handle all worker messages from the window thread.
    * @param event Message event.
    */
-  #onMessage(event: MessageEvent<Array<WorkerValueTypes>>) {
+  #onMessage(event: MessageEvent<Array<ValueTypes>>) {
     if (event.data instanceof Array) {
-      const [name, ...values] = event.data as [string, ...WorkerValueTypes[]];
-      const events = this.#events[name];
-      if (events !== void 0) {
-        for (const callback of events) {
-          callback(...values);
-        }
+      const [id, name, ...values] = event.data as [number, string, ...ValueTypes[]];
+      if (name !== '') {
+        this.#notifyListeners(id, name, ...values);
+      } else {
+        this.#notifyPromises(id, ...values);
       }
     }
   }
@@ -58,20 +145,47 @@ export class Worker {
    * Emit a new event to the window thread.
    * @param name Event name.
    * @param values Event values.
+   * @returns Returns a new promise for the getting the result.
    */
-  emit(name: string, ...values: WorkerValueTypes[]): void {
-    this.#thread.postMessage([name, ...values]);
+  async emit(name: string, ...values: ValueTypes[]): Promise<ValueTypes[]> {
+    Worker.#assertName(name);
+    return new Promise((resolve, reject) => {
+      try {
+        const id = Worker.#promiseIds++;
+        Worker.#eventPromises[id] = { resolve, reject };
+        this.#thread.postMessage([id, name, ...values]);
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
   /**
-   * Register a new listener for events that corresponds to the given name.
+   * Register a new event listener for the given name.
    * @param name Event name.
    * @param callback Listener callback.
    */
-  listen(name: string, callback: WorkerEventCallback) {
-    if (!(this.#events[name] instanceof Array)) {
-      this.#events[name] = [];
+  listen(name: string, callback: EventCallback): void {
+    Worker.#assertName(name);
+    if (!(this.#eventListeners[name] instanceof Array)) {
+      this.#eventListeners[name] = [];
     }
-    this.#events[name].push(callback);
+    this.#eventListeners[name].push(callback);
+  }
+
+  /**
+   * Remove the event listener that corresponds to the given name and callback.
+   * @param name Event name.
+   * @param callback Listener callback.
+   */
+  unlisten(name: string, callback: EventCallback): void {
+    Worker.#assertName(name);
+    const events = this.#eventListeners[name];
+    if (events instanceof Array) {
+      const index = events.indexOf(callback);
+      if (index !== -1) {
+        events.splice(index, 1);
+      }
+    }
   }
 }
